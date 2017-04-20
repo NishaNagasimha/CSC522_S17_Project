c810a640c3d7dfe3d15497cd910b8eb2f56a16c4
ruthvik1994 <mruthvik1994@gmail.com>
2017-04-20 13:10:26 -0400

    Deleted copy file

--- KNN-classifier.R
+++ KNN-classifier.R
@@ -99,7 +99,7 @@ get_error <- function(k, predData){
 
 #kval <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
 # Takes about 10mins
-kval <- c(3,5,6,8,10,12)
+kval <- c(21,22,23,24,25,26,27,28,29.30)
 sse <- vector()
 for(k in kval){
   print (k)
