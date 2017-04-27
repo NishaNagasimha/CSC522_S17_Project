0713d1284ee4b99aa1c1e7c52cd1db36b3ff23b4
nknagasi <nknagasi@ncsu.edu>
2017-04-26 23:31:14 -0400

    KMeans Clustering

--- PoC_KMeans/Kmeans.py
+++ PoC_KMeans/Kmeans.py
@@ -1,1 +1,173 @@
 #Code to recommend genres of the users using KMeans
+#KMeans
+
+import re
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from sklearn.cluster import KMeans
+from scipy.stats.stats import pearsonr
+from itertools import groupby
+from collections import Counter
+import time
+#import csv
+
+genre = []
+
+
+u = []
+def getUser(file):
+	f = open(file, "r")
+	text = f.read()
+	u_records = re.split("\n+", text)
+	for record in u_records:
+		val = record.split('|', 5)
+		u.append(val)
+	return u
+
+i = []
+def getItem(file):
+	f = open(file, "r")
+	text = f.read()
+	i_records = re.split("\n+", text)
+	for record in i_records:
+		val = record.split('|', 24)
+		i.append(val)
+	return i
+
+
+r = []
+def getRating(file):
+	f = open(file, "r")
+	text = f.read()
+	r_records = re.split("\n+", text)
+	for record in r_records:
+		val = record.split('\t', 4)
+		r.append(val)
+	return r
+
+users = getUser("u.user")
+movies = getItem("u.item")
+rating = getRating("u.base")
+#test = getRating("u1.test")
+rating.pop()
+
+a = len(users)
+
+b = len(movies)
+
+l = len(rating)
+
+#t = len(test)
+
+#test_matrix = np.zeros((a,b))
+
+'''for t in test:
+	print t
+	k = int(t[0])
+	j = int(t[1])
+	res = int(t[2])
+	#print "k", k
+	#print "j",j
+	#print r
+	test_matrix[k][j] = res'''
+
+
+user_movie_matrix = np.zeros((a,b))
+
+#user_movie_matrix = [[0 for x in range(b)] for y in range(b)]
+#print user_movie_matrix[943][1682]
+
+for r in rating:
+	k = int(r[0])
+	j = int(r[1])
+	res = int(r[2])
+	#print "k", k
+	#print "j",j
+	#print r
+	user_movie_matrix[k][j] = res
+
+movies.pop()
+
+genre = []
+for movie in movies:
+	item =[]
+	for i in range(5,24):
+		item.append(movie[i])
+	genre.append(item)
+
+genre = np.array(genre)
+
+#kmeans_cluster = KMeans(n_clusters=19, random_state=0).fit_predict(genre)
+
+#print kmeans_cluster[1]
+
+cluster = KMeans(n_clusters=19, random_state=0).fit_predict(genre)
+
+genre_name = ["unknown", "action", "adventure", "animation", "childrens", "comedy", "crime", "documentary","drama", "fantasy", "film_noir", "horror", "musical", "mystery","romance", "sci_fi", "thriller", "war", "western"]
+
+recommend = {}
+
+c = b-1
+
+for i in range(0,a):
+	g_list = []
+	for j in range(0,c):
+		if user_movie_matrix[i][j] >= 3:
+			g_list.append(cluster[j])
+	recommend[i] = g_list
+
+final = {}
+
+for k, v in recommend.iteritems():
+	rec = {}
+	#tuple = (element, count)
+	rec = Counter(v).most_common(len(v))
+	length = len(rec)
+	top5 = []
+
+	if length < 5:
+		for i in range(0,length):
+			top5.append(rec[i])
+	else:
+		for i in range(0,5):	
+			top5.append(rec[i])
+			
+		
+	final[k] = top5
+		
+#print final
+
+top_genre = {}
+
+for k, v in final.iteritems():
+	gen = []
+	for x in v:
+		z = x[0]
+		gen.append(genre_name[z])
+
+	top_genre[k] = gen
+
+for k, v in top_genre.iteritems():
+	print "User id: ",k
+	for y in range(0, len(v)):
+		print "Top ",(y+1)," genre: ", v[y]
+		#time.sleep(0.1)
+	#time.sleep(0.9)
+'''
+cluster = KMeans(n_clusters=2)
+print cluster.fit_predict(genre)
+
+for i in range(a):
+	for j in range(b):
+		print user_movie_matrix[i][j]
+
+# Calculating mean squared error
+actual_y = []
+y_pred = []
+
+for i in range(0, a):
+    for j in range(0, b):
+        actual_y.append(test_matrix[i][j])
+        y_pred.append(user_movie_matrix[i][j])
+print "Mean Squared Error: %f" % mean_squared_error(actual_y, y_pred)'''
+
